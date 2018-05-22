f9ea3567b538490cf00ae60b51045ba573816b71
chenxiaohu1986 <523940697@qq.com>
2018-05-22 15:15:04 +0800

    a

--- app/src/main/java/com/ihandy/wyreader/view/base/BaseFragment.java
+++ app/src/main/java/com/ihandy/wyreader/view/base/BaseFragment.java
@@ -1,10 +1,17 @@
 package com.ihandy.wyreader.view.base;
 
 import android.content.Context;
+import android.databinding.DataBindingUtil;
+import android.databinding.ViewDataBinding;
 import android.support.annotation.LayoutRes;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.ihandy.wyreader.viewmodel.BaseViewModel;
+
+import butterknife.ButterKnife;
 
 /**
  * Created by cxh on 18/5/22.
@@ -12,9 +19,9 @@ import com.ihandy.wyreader.viewmodel.BaseViewModel;
 
 public class BaseFragment extends Fragment {
 
-
+	protected BaseViewModel mModel;
 	protected Context mContext;
-
+	private View mBindView;
 
 	/**
 	 * 获得全局的，防止使用getActivity()为空
@@ -31,11 +38,18 @@ public class BaseFragment extends Fragment {
 	 * Databinding设置布局绑定
 	 *
 	 * @param resId      布局layout
-	 * @param brVariavle BR或者不用绑定
+	 * @param brVariavle BR
 	 * @param mModel     viewmodel
 	 */
-	public void setBinddingView(@LayoutRes int resId, int brVariavle, BaseViewModel mModel) {
-
+	public View setBinddingView(LayoutInflater inflater, ViewGroup container, @LayoutRes int resId, int brVariavle, BaseViewModel mModel) {
+		if (mBindView == null){
+			ViewDataBinding dataBinding = DataBindingUtil.inflate(inflater,resId,container,false);
+			dataBinding.setVariable(brVariavle,mModel);
+			mBindView = dataBinding.getRoot();
+			ButterKnife.bind(this,mBindView);
+			this.mModel = mModel;
+		}
+		return mBindView;
 	}
 
 
